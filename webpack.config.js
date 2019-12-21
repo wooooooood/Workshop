66d10363ad7e0f13f639abbd4654054d4f97d201
g0 <g0lim@kakao.com>
2019-12-21 15:43:20 +0900

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //webpack configuration file, 존나 수정 예정
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //src의 파일을 public에 만드러줌
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     entry: './src/index.js',  //쉼표, 중괄호, 세미콜론 중요
@@ -22,6 +23,20 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new WebpackPwaManifest({
+            name: "리액트 메모",
+            short_name: "메모",
+            description: "리액트로 제작한 메모앱입니다.",
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials',
+            theme_color: '#eeeeee',
+            icons: [
+                {
+                    src: path.resolve('src/assets/Icon.png'),
+                    sizes: [96,128,192,256,384,512]
+                }
+            ]
         })
     ]
 }
