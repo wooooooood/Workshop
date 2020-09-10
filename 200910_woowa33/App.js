abbe34bf7210bd112a912aae96c5846bb14e6d85
wooooooood <13pft13@gmail.com>
2020-09-10 21:14:45 +0900

    react 훅

--- 200910_woowa33/App.js
+++ 200910_woowa33/App.js
@@ -12,34 +12,33 @@ class ClassApp extends React.Component {
     };
   }
 
-  onToggleDisplayOrder = () => {
+  toggleDisplayOrder = () => {
     this.setState({
       //애로우는 raxical? 문맥 컨텍스트 따름. 다른건 실행 컨택스트 따라서 바인딩을 해야함. 애로우는 바인딩 필요없음
       displayOrder: displayOrder === "ASC" ? "DESC" : "ASC"
     });
   };
 
-  toggleDisplayOrder = () => {};
-
   render() {
     return (
       <div>
         여기여기
-        <button onClick={onToggleDisplayOrder}>정렬</button>
+        <button onClick={this.toggleDisplayOrder}>정렬</button>
       </div>
     );
   }
 }
 
 const App = (props) => {
+  const { displayOrder, toggleDisplayOrder } = React.useState("ASC");
   const { sessionList } = props.store;
   const orderedSessionList = sessionList.map((session, i) => ({
     ...session,
-    order: 1
+    order: i
   }));
 
-  const toggleDisplayOrder = () => {
-    displayOrder = displayOrder === "ASC" ? "DESC" : "ASC";
+  const onToggleDisplayOrder = () => {
+    toggleDisplayOrder(displayOrder === "ASC" ? "DESC" : "ASC");
   };
 
   return (
@@ -47,8 +46,8 @@ const App = (props) => {
       <header>
         <h1>React and TypeScript</h1>
       </header>
-      <p>전체 세션 갯수: 4개</p>
-      <button>재정렬</button>
+      <p>전체 세션 갯수: 4개 {displayOrder}</p>
+      <button onClick={onToggleDisplayOrder}>재정렬</button>
       <ul>
         {orderedSessionList.map((session) => (
           <SessionItem title={session.title} />
--- 200910_woowa33/index.js
+++ 200910_woowa33/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const sessionList = [
   { title: "1회차: Overview", order: 1 },
-  { title: "2회차: Redux 만들기", order: 1 },
-  { title: "3회차: React 만들기", order: 1 },
-  { title: "4회차: 컴포넌트 디자인 및 비동기", order: 1 }
+  { title: "2회차: Redux 만들기", order: 2 },
+  { title: "3회차: React 만들기", order: 3 },
+  { title: "4회차: 컴포넌트 디자인 및 비동기", order: 4 }
 ];
 ReactDOM.render(
   <React.StrictMode>
