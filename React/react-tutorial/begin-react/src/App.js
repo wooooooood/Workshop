c57356a92ad3a61216375fca0263a77f166ebc29
wooooooood <13pft13@gmail.com>
2020-10-11 23:17:53 +0900

    props 구조분해 및 기본값 설정

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -3,7 +3,10 @@ import Hello from './Hello.js';
 
 function App() {
   return (
+    <>
       <Hello name="React" color="red"/>
+      <Hello color="pink"/>
+    </>
   );
 }
 
--- React/react-tutorial/begin-react/src/Hello.js
+++ React/react-tutorial/begin-react/src/Hello.js
@@ -3,12 +3,16 @@ import React from 'react'; //react를 불러와서 사용하겠다
 //컴포넌트 만들기: 함수형 or 클래스
 
 //컴포넌트 이름은 파스칼케이스
-function Hello(props) {
+function Hello({color, name}) {
     //jsx 리턴
     //객체, 객체를 감싸는 중괄호 
     return <div style={{
-        color: props.color
-    }}>ㅎㅇ {props.name}</div>;
+        color
+    }}>ㅎㅇ {name}</div>;
 }
+
+Hello.defaultProps = {
+    name: '이름없음'
+};
 
 export default Hello;
