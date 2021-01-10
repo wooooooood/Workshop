b4ec38a02794f3dde7dc15fbd959428454a27c19
wooooooood <13pft13@gmail.com>
2021-01-10 21:39:31 +0900

    useRef로 컴포넌트 안의 변수 만들기
    - setTimeout, setInterval의 id
    - 외부 라이브러리를 사용하여 생성된 인스턴스
    - scroll 위치 등

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from './UserList';
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username: 'v',
+        email: 'v@v.com'
+    },
+    {
+        id: 2,
+        username: 'v2',
+        email: 'v2@v.com'
+    },
+    {
+        id: 3,
+        username: 'v3',
+        email: 'v3@v.com'
+    },
+  ];
+
+  //값이 바뀐다고 컴포넌트가 리렌더링 될 필요가 없기 때문에 변수로 관리하는것도 괜찬다
+  const nextId = useRef(4);
+  const onCreate = () => {
+    console.log(nextId.current); //4
+    nextId.current += 1;
+  }
+
   return (
-    <UserList />
+    <UserList users={users}/>
   );
 }
 
