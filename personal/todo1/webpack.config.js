b298aa1c1ad48387b9c5dad9adc4337af37a0699
wooooooood <13pft13@gmail.com>
2021-10-03 15:06:18 +0900

    webpack default setup

---
+++ personal/todo1/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const mode = process.env.NODE_ENV || "development";
+module.exports = () => {
+  return {
+    mode,
+    entry: {
+      main: "./src/index.tsx",
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve("./dist"),
+      assetModuleFilename: "images/[hash][ext][query]",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+        minify:
+          mode === "production"
+            ? {
+                collapseWhitespace: true, // 빈칸 제거
+                removeComments: true, // 주석 제거
+              }
+            : false,
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+        },
+        {
+          test: /\.(png|jpg)$/,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(svg)$/,
+          type: "asset/inline",
+        },
+      ],
+    },
+    devServer: {
+      host: "localhost",
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".scss", ".json"],
+      alias: {
+        // "@stories": path.resolve(__dirname, "./src/stories"),
+      },
+    },
+  };
+};
