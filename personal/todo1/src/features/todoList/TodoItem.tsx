9578fbf36ec10d144e889c85eb0bc0b243a05e2a
wooooooood <13pft13@gmail.com>
2021-10-04 19:27:01 +0900

    hover 안에 저거 또 못하나?

--- personal/todo1/src/features/todoList/TodoItem.tsx
+++ personal/todo1/src/features/todoList/TodoItem.tsx
@@ -1,5 +1,5 @@
 import { useAppSelector, useAppDispatch } from "@app/hooks";
-import { MdDone, MdDelete } from "react-icons/md";
+import { MdDone, MdDelete, MdEdit } from "react-icons/md";
 import { todoItem, toggle, remove } from "./todoListSlice";
 import styled, { css } from "styled-components";
 
@@ -7,13 +7,19 @@ const TodoItem = ({ id, text, done }: todoItem) => {
   const dispatch = useAppDispatch();
   const onToggle = () => dispatch(toggle(id));
   const onRemove = () => dispatch(remove(id));
+  const onEdit = () => dispatch(remove(id));
 
   return (
     <ItemBlock>
       <div className="check-box" onClick={onToggle}>
         {done && <MdDone />}
       </div>
-      <p>{text}</p>
+      <div>
+        <p className="text">{text}</p>
+        <div className="edit-icon" onClick={onEdit}>
+          <MdEdit />
+        </div>
+      </div>
       <div onClick={onRemove}>
         <MdDelete />
       </div>
@@ -29,8 +35,26 @@ const ItemBlock = styled.div`
   .check-box {
     width: 20px;
     height: 20px;
+    font-size: 20px;
     border: 1px solid gray;
     border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .text {
+    display: inline-flex;
+    &:hover {
+      .edit-icon {
+        opacity: 1;
+        color: red;
+      }
+    }
+  }
+
+  .edit-icon {
+    display: inline-flex;
+    opacity: 0.3;
+    color: blue;
   }
 `;
 
